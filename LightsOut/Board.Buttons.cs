dba400fa044f1c8c7ea2ac711a57a3b38f03ee7e
GlassToeStudio <GlassToeStudio@gmail.com>
2024-11-03 16:44:29 -0600

    Move button events to Board.Buttons class

---
+++ LightsOut/Board.Buttons.cs
@@ -0,0 +1,36 @@
+﻿namespace LightsOut
+{
+    partial class frmLightsOut
+    {
+        private void btnLight_Click(object sender, EventArgs e)
+        {
+            Light? light = sender as Light;
+            light?.ClickLight();
+
+            levelData.UpdateBoard(lights);
+            UpdateMoves();
+            CheckWin();
+        }
+
+        private void btnSolve_Click(object sender, EventArgs e)
+        {
+            SolvePuzzle();
+        }
+
+        private void btnSolveOne_Click(object sender, EventArgs e)
+        {
+            SolveOne();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            GenerateRandomLevel();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            GenerateLevelFromFile();
+        }
+
+    }
+}
--- LightsOut/Board.cs
+++ LightsOut/Board.cs
@@ -165,36 +165,6 @@ namespace LightsOut
             CheckWin();
         }
 
-        private void btnLight_Click(object sender, EventArgs e)
-        {
-            Light? light = sender as Light;
-            light?.ClickLight();
-
-            levelData.UpdateBoard(lights);
-            UpdateMoves();
-            CheckWin();
-        }
-
-        private void btnSolve_Click(object sender, EventArgs e)
-        {
-            SolvePuzzle();
-        }
-
-        private void btnSolveOne_Click(object sender, EventArgs e)
-        {
-            SolveOne();
-        }
-
-        private void btnGenerate_Click(object sender, EventArgs e)
-        {
-            GenerateRandomLevel();
-        }
-
-        private void btnLoad_Click(object sender, EventArgs e)
-        {
-            GenerateLevelFromFile();       
-        }
-
         private string DebugBoardState()
         {
             String output = "";
