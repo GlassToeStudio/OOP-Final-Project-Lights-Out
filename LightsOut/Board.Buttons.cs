bfe544a79cb467ca10a01d90dda251017eb5fb61
GlassToeStudio <GlassToeStudio@gmail.com>
2024-11-03 16:49:01 -0600

    Fix board.buttons

--- LightsOut/Board.Buttons.cs
+++ LightsOut/Board.Buttons.cs
@@ -1,36 +1,41 @@
-﻿namespace LightsOut
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightsOut
 {
     partial class frmLightsOut
     {
-        private void btnLight_Click(object sender, EventArgs e)
-        {
-            Light? light = sender as Light;
-            light?.ClickLight();
+		private void btnLight_Click(object sender, EventArgs e)
+		{
+			Light? light = sender as Light;
+			light?.ClickLight();
 
-            levelData.UpdateBoard(lights);
-            UpdateMoves();
-            CheckWin();
-        }
+			levelData.UpdateBoard(lights);
+			UpdateMoves();
+			CheckWin();
+		}
 
-        private void btnSolve_Click(object sender, EventArgs e)
-        {
-            SolvePuzzle();
-        }
+		private void btnSolve_Click(object sender, EventArgs e)
+		{
+			SolvePuzzle();
+		}
 
-        private void btnSolveOne_Click(object sender, EventArgs e)
-        {
-            SolveOne();
-        }
+		private void btnSolveOne_Click(object sender, EventArgs e)
+		{
+			SolveOne();
+		}
 
-        private void btnGenerate_Click(object sender, EventArgs e)
-        {
-            GenerateRandomLevel();
-        }
+		private void btnGenerate_Click(object sender, EventArgs e)
+		{
+			GenerateRandomLevel();
+		}
 
-        private void btnLoad_Click(object sender, EventArgs e)
-        {
-            GenerateLevelFromFile();
-        }
-
-    }
+		private void btnLoad_Click(object sender, EventArgs e)
+		{
+			GenerateLevelFromFile();
+		}
+	}
 }
