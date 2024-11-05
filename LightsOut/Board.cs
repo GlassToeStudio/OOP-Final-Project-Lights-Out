dacddba4861aaacb7ff14beeca9073868c266e7f
GlassToeStudio <GlassToeStudio@gmail.com>
2024-11-05 14:00:03 -0600

    Move all Button code back into one file.
    Will redistribute as necesary.

--- LightsOut/Board.cs
+++ LightsOut/Board.cs
@@ -20,11 +20,8 @@ namespace LightsOut
             InitializeComponent();
             PreloadLevelsData();
             levelData = new LevelData().LoadLevelDataFromJson(levelName);
-
-            
             GenerateGameBoard();
-
-            //GenerateLevelFromFile();
+            GenerateLevelFromFile();
         }
 
         private void PreloadLevelsData()
@@ -38,7 +35,88 @@ namespace LightsOut
             cbxLevelSelect.SelectedIndex = 0;
 
         }
+        private void GenerateGameBoard()
+        {
+            gbxGameBoard_3x3.Visible = false;
+            gbxGameBoard_4x4.Visible = false;
+            //Light[] lights_5x5 = new Light[25];
 
+            Light[] lights_4x4 = [
+                        light_4x4_00, light_4x4_01, light_4x4_02, light_4x4_03,
+                        light_4x4_10, light_4x4_11, light_4x4_12, light_4x4_13,
+                        light_4x4_20, light_4x4_21, light_4x4_22, light_4x4_23,
+                        light_4x4_30, light_4x4_31, light_4x4_32, light_4x4_33];
+
+            Light[] lights_3x3 = [
+                        light_3x3_00, light_3x3_01, light_3x3_02,
+                        light_3x3_10, light_3x3_11, light_3x3_12,
+                        light_3x3_20, light_3x3_21, light_3x3_22];
+
+            switch (levelData.Size)
+            {
+                case 3:
+                    gbxGameBoard_3x3.Visible = true;
+                    lights = lights_3x3;
+                    break;
+                case 4:
+                    gbxGameBoard_4x4.Visible = true;
+                    lights = lights_4x4;
+                    break;
+                default:
+                    gbxGameBoard_4x4.Visible = true;
+                    lights = lights_4x4;
+                    break;
+            }
+
+            InitializeBoardLights();
+            ConnectNeighbors();
+        }
+
+        private void InitializeBoardLights()
+        {
+            // Set On off button and index value
+            for (var i = 0; i < lights.Length; i++)
+            {
+                lights[i].Neighbors.Clear();
+                lights[i].SetButtons(OnButton, OffButton);
+                lights[i].index = i;
+                lights[i].TurnOff();
+            }
+        }
+        private void ConnectNeighbors()
+        {
+            // Connect neighbors
+            var size = lights.Length;
+
+            for (int pos = 0; pos < size; pos++)
+            {
+                var n1 = pos - levelData.Size;
+                var n2 = pos + levelData.Size;
+
+                var n3 = pos - 1;
+                var n4 = pos + 1;
+
+                if (n1 >= 0)
+                {
+                    lights[pos].AddNeighbor(lights[n1]);
+                }
+
+                if (n2 < size)
+                {
+                    lights[pos].AddNeighbor(lights[n2]);
+                }
+
+                if (pos % levelData.Size != 0)
+                {
+                    lights[pos].AddNeighbor(lights[n3]);
+                }
+
+                if (n4 % levelData.Size != 0)
+                {
+                    lights[pos].AddNeighbor(lights[n4]);
+                }
+            }
+        }
         private void GenerateLevelFromFile()
         {
             moves = 0;
@@ -237,12 +315,82 @@ namespace LightsOut
             {
                 return 4;
             }
-            else if(rb5x5.Checked)
+            else if (rb5x5.Checked)
             {
                 return 5;
             }
             return 4;
         }
 #endif
+
+        #region Buttons
+        private void Light_Click(object sender, EventArgs e)
+        {
+            Light? light = sender as Light;
+            OnCLickLight(light);
+            CheckWin();
+        }
+
+        private void LoadLevel_Click(object sender, EventArgs e)
+        {
+            GenerateLevelFromFile();
+            UpdateUI();
+            EnableLights();
+        }
+#if DEBUG
+
+        private void SolveAll_Click(object sender, EventArgs e)
+        {
+            SolvePuzzle();
+        }
+
+        private void SolveOne_Click(object sender, EventArgs e)
+        {
+            SolveOne();
+        }
+
+        private void GenerateRandom_Click(object sender, EventArgs e)
+        {
+            GenerateRandomLevel();
+            UpdateUI();
+            EnableLights();
+        }
+
+        private void ShowHideDebug_Click(object sender, KeyPressEventArgs e)
+        {
+            switch (e.KeyChar.ToString())
+            {
+                case "d":
+                case "D":
+                    if (debug)
+                    {
+                        debug = false;
+                        this.Width = 500;
+                    }
+                    else
+                    {
+                        debug = true;
+                        this.Width = 675;
+                    }
+
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void SaveLevelToFile_Click(object sender, EventArgs e)
+        {
+            levelData = new LevelData(levelData);
+            var solution = Solver.GetSolutionMatrix(levelData);
+            levelData.MinMoves = solution.Sum();
+            string ProjectDir = "C:\\Users\\GlassToe\\Documents\\Calhoun Comminity College\\Fall 24\\CIS 285 - Object-Oriented Programming (11022)\\Final Project\\OOP-Final-Project-Lights-Out\\LightsOut\\Resources\\Levels\\";
+            var data = JsonConvert.SerializeObject(levelData);
+            File.WriteAllText(FileUtil.GetLevelFile($"Levels_{level}.json"), data);
+            File.WriteAllText($"{ProjectDir}Levels_{level}.json", data);
+            PreloadLevelsData();
+        }
+#endif
+        #endregion
     }
 }
