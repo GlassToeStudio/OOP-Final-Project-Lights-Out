6fc524e48f8c287c40f0ff8b7efe38c89f2f510d
GlassToeStudio <GlassToeStudio@gmail.com>
2024-11-12 13:25:27 -0600

    Add doc comments for LightState

--- LightsOut/LightState.cs
+++ LightsOut/LightState.cs
@@ -1,13 +1,13 @@
 ﻿namespace LightsOut
 {
     /// <summary>
-    /// Enum to indicate in what state a <seealso cref="Light"/> is in.
+    /// Enum to indicate in what state a <see cref="Light"/> is in.
     /// </summary>
     public enum LightState
     {
-        /// <summary>Indicates that this <seealso cref="Light"/> is in the Off state.</summary>
+        /// <summary>Indicates that this <see cref="Light"/> is in the Off state.</summary>
         Off = 0,
-        /// <summary>Indicates that this <seealso cref="Light"/> is in the On state.</summary>
+        /// <summary>Indicates that this <see cref="Light"/> is in the On state.</summary>
         On = 1
     }
 }
