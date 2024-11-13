b69599c96c4b42489626193236693da24c0efdb7
GlassToeStudio <GlassToeStudio@gmail.com>
2024-11-13 09:19:30 -0600

    Update docstrings in level database (previously AllLevels)

--- LightsOut/LevelDatabase.cs
+++ LightsOut/LevelDatabase.cs
@@ -13,18 +13,36 @@
         /// The current index of the selected level in the database.
         /// </summary>
         public int SelectedIndex { get; set; }
+        /// <summary>
+        /// Number of levels stored in database
+        /// </summary>
         public int Count => Levels.Length;
+        /// <summary>
+        /// Next index in database.
+        /// </summary>
         public int Next => Math.Min(SelectedIndex + 1, this.Count - 1);
+        /// <summary>
+        /// Previous index in database
+        /// </summary>
         public int Previous => Math.Max(SelectedIndex - 1, 0);
+        /// <summary>
+        /// The current item selected from database.
+        /// </summary>
         public LevelData SelectedItem => this[SelectedIndex];
 
-
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
         public LevelDatabase()
         {
             Levels = [];
         }
 
-
+        /// <summary>
+        /// Get level from database by index.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
         public LevelData this[int index]
         {
             get
