28d331edc8d7271b0e8762c11e955a291418d81d
GlassToeStudio <GlassToeStudio@gmail.com>
2024-11-17 10:30:42 -0600

    Update data handler comments

--- LightsOut/DataHandler.cs
+++ LightsOut/DataHandler.cs
@@ -9,10 +9,20 @@
     /// </summary>
     public class DataHandler()
     {
-        bool manualLoad = false;
         /// Load UserDatabase and GameDatabase from disk.
         private readonly UserDatabase user = new UserDatabase().LoadUserDatabase();
         private LevelDatabase game = new LevelDatabase().LoadLevelDatabase();
+
+#if DEBUG
+        /// <summary>
+        /// Bypass editing and saving user data when we manually load a level.
+        /// If we load a level manually, i.e. from the dropdown box, we may have
+        /// loaded a level that is not unlocked yet. We do not want to perform
+        /// the usual operations of the user data if we do this. So we can bypass
+        /// savind data, and editing user data by use of this flag.
+        /// </summary>
+        private bool manualLoad = false;
+#endif
 
         /// <summary>
         /// Get the GameDatabase object which holds all pre-made loaded levels.
@@ -34,7 +44,6 @@
         /// The level currenttly loaded from the UserDatabase.
         /// </summary>
         public LevelData CurrentLevel => user.CurrentLevel;
-
         /// <summary>
         /// Get a new LevelData object of the current loaded level from the UserDatabse.
         /// </summary>
@@ -42,6 +51,12 @@
         {
             get
             {
+                /* Normally we load the level based on the SelectedIndex that is
+                 * Controlled through UI buttons and has checks in place to oly
+                 * attmept to load a valid unlocked level.
+                 * Manually loading a level would cause and index out of range error.
+                 * We can check for that here and set the manualLoad flag to true if so.
+                 */
                 try
                 {
                     manualLoad = false;
@@ -54,13 +69,13 @@
                 }
             }
         }
-        
+
         /// <summary>
         /// Increment the SelectedIndex by one, this will progress to the next unlocked level from the UserDatabase.
         /// </summary>
         public void IncrementLevel()
         {
-            if( (user.MaxIndex > user.SelectedIndex)&& (user.SelectedIndex + 1 < game.Levels.Count - 1))
+            if ((user.MaxIndex > user.SelectedIndex) && (user.SelectedIndex + 1 < game.Levels.Count - 1))
             {
                 user.SelectedIndex++;
             }
@@ -85,19 +100,21 @@
         /// </summary>
         /// <param name="moves">The number of moves taken to beat this level.</param>
         /// <returns>A new LevelData object with updated BestScore and Stars.</returns>
-        public LevelData UpdateUserData( int moves)
+        public LevelData UpdateUserData(int moves)
         {
-            if((MaxIndex + 1 < Levels.Count-1) &&(MaxIndex == SelectedIndex))
+            if ((MaxIndex + 1 < Levels.Count) && (MaxIndex == SelectedIndex))
             {
                 user.MaxIndex += 1;
                 user.AddLevel(Levels[MaxIndex]);
             }
-            
+
             var levelData =  new LevelData(Levels[SelectedIndex], moves);
+#if DEBUG
             if (manualLoad)
             {
                 return levelData;
             }
+#endif
 
             if (CurrentLevel.BestScore > levelData.BestScore)
             {
@@ -112,10 +129,12 @@
         /// </summary>
         public void SaveUserData()
         {
+#if DEBUG
             if (manualLoad)
             {
                 return;
             }
+#endif
             FileUtil.SaveUserData(user);
         }
 
@@ -123,8 +142,9 @@
         /// <summary>
         /// Manually change the SelectedIndex to load a level.
         /// </summary>
-        /// <param name="index"></param>
-        public void UpdateIndex(int index) {
+        /// <param name="index">index in Levels List that we wish to load.</param>
+        public void UpdateIndex(int index)
+        {
             user.SelectedIndex = index;
         }
 #endif
